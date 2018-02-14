f67240c6918e0325af466291e33d3e45e44322fc
AlejandraCP <alejandracp710@gmail.com>
2018-02-14 13:03:04 -0500

    Adding name personages

--- js/home.js
+++ js/home.js
@@ -1,25 +1,42 @@
 let btnWelcome = $('#btn-welcome');
-let  imagesBox = $('.images-box');
-
+let imagesBox = $('#images-box');
 btnWelcome.on('click',function(){
   window.location.href = '../views/home.html';
 });
 
 // función para ingresar imágenes
 
-
-
-
-function getPersonage() {
+getPersonage = () => {
   const personageStar = new XMLHttpRequest();
   personageStar.onreadystatechange = function() {
     if (personageStar.readyState === 4 && personageStar.status === 200) {
       const data = JSON.parse(this.responseText);
+      let dataArray = data.results;
       console.log(data);
+      resultLength = data.results.length;
+      personageStar.onload = addPersonage(dataArray);
+      personageStar.onerror = handleError;
     }
   };
   personageStar.open('GET', `https://swapi.co/api/people/`);
   personageStar.send();
 }
 
+addPersonage = (array) => {
+  array.forEach((element) => {
+    const template = `
+    <div>
+    <p>${element.name}</p>
+    </div>
+    `;
+    imagesBox.append(template);
+    console.log(element.name);
+  })
+};
+
+handleError = error => {
+  console.log(error);
+};
+
 getPersonage();
+
